5106ad17131aea5363f7fffc9a9a19c15f66d7bd
lewisgabriel <lewisgbriel8@gmail.com>
2020-06-15 22:19:59 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <Header />
+    <HeroHeader />
+    <SliderProducts />
+    <InstaView />
+    <LogoPartner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
+import HeroHeader from '@/components/HeroHeader.vue';
+import SliderProducts from '@/components/SliderProducts.vue';
+import InstaView from '@/components/InstaView.vue';
+import LogoPartner from '@/components/LogoPartner.vue'
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'home',
+  components: {
+      Header,
+      HeroHeader,
+      SliderProducts,
+      InstaView,
+      LogoPartner,
+      Footer
+  }
+}
+</script>
+
